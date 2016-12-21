2e9d1226365f91e9e6f66cf78889368604efbb3e
Amanda Prorok <amanda@Amandas-MBP.mrsl>
2016-12-21 15:20:41 -0500

    First commit.

---
+++ private_vrp.py
@@ -0,0 +1,51 @@
+
+
+# Standard modules
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+verbose = False
+
+# Global settings
+grid_size = 2
+num_nodes = grid_size**2
+num_vehicles = 4
+num_passengers = num_vehicles
+
+
+# Load graph
+graph = nx.grid_2d_graph(grid_size, grid_size, periodic=False, create_using=None)
+print "Number of nodes in graph: " , nx.number_of_nodes(graph)
+print nx.info(graph)
+
+print graph.nodes()
+print graph.edges()
+
+#print nx.all_neighbors(graph,0)
+#nx.draw(graph)
+#plt.show()
+
+
+# Initialization
+#vehicle_pos_init = np.array((num_nodes))
+vehicle_node_init = np.random.choice(np.arange(num_nodes), size=num_vehicles, replace=False)
+passenger_node_init = np.random.choice(np.arange(num_nodes), size=num_passengers, replace=False)
+
+print vehicle_node_init
+print passenger_node_init
+
+
+sp = nx.shortest_path(graph, source=graph.nodes()[0], target=graph.nodes()[1])
+print sp
+
+# Compute cost matrix (vehicle to passenger pairings)
+allocation_cost = np.zeros((num_vehicles, num_passengers))
+for i in range(num_vehicles):
+	for j in range(num_passengers):
+		# Compute cost of shortest path for all possible allocations
+		sp = nx.shortest_path(graph, source=graph.nodes()[vehicle_node_init[i]], target=graph.nodes()[passenger_node_init[j]])
+		#allocation_cost[i, j] = sp
+
+
+# Find optimal allocation (Munkres algorithm)
