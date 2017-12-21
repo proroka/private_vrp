69e09a9f142dff65c6c1d632c6c6405dac32a460
Amanda Prorok <amanda.prorok@gmail.com>
2017-12-21 10:24:03 +0000

    changes to pandas plot function.

--- plot_batch_rich-vrp-nice.py
+++ plot_batch_rich-vrp-nice.py
@@ -24,7 +24,9 @@ for r in runs:
         sampled_cost = data['sampled_cost']
 
     data = []
-    column_names = ('algorithm', 'epsilon', 'max_assignable_vehicles', 'iteration', 'passenger', 'waiting_time')
+    num_iterations = None
+    num_passengers = None
+    column_names = ('algorithm', 'epsilon', 'max_assignable_vehicles', 'iteration', 'passenger', 'iteration_passenger', 'waiting_time')
     for algorithm_name, d in waiting_time.iteritems():
         tokens = algorithm_name.split('_', 3)
         if len(tokens) == 1:
@@ -35,16 +37,29 @@ for r in runs:
             algorithm, epsilon, _ = tokens
             epsilon = float(epsilon)
         for max_assignable_vehicles, run_values in d.iteritems():
+            if num_iterations is not None:
+                assert num_iterations == len(run_values)
+            else:
+                num_iterations = len(run_values)
             for run_index, waiting_times in enumerate(run_values):
-                for p, waiting_time in enumerate(waiting_times):
-                    data.append((algorithm, epsilon, max_assignable_vehicles, run_index + run_index_offset, p, waiting_time))
-            run_index_offset += len(run_values)
+                if num_passengers is not None:
+                    assert num_passengers == len(waiting_times)
+                else:
+                    num_passengers = len(waiting_times)
 
+                for p, waiting_time in enumerate(waiting_times):
+                    iteration_index = run_index + run_index_offset
+                    data.append((algorithm, epsilon, max_assignable_vehicles, iteration_index, p, p + iteration_index * num_passengers, waiting_time))
+    
+    run_index_offset += num_iterations
     dataframes.append(pd.DataFrame(data, columns=column_names))
 
-df = pd.concat(dataframes)
+df = pd.concat(dataframes, ignore_index=True)
 
 # sns.violinplot(data=df, x='algorithm', y='waiting_time')
-#sns.tsplot(data=df, time='max_assignable_vehicles',
-#           condition='algorithm', value='waiting_time')
+df = df[(df.epsilon == 100) | pd.isnull(df.epsilon)]
+df = df.groupby('iteration').waiting_time.mean()
+
+sns.tsplot(data=df, time='max_assignable_vehicles',
+           value='waiting_time', condition='algorithm', unit='iteration')
 plt.show()
